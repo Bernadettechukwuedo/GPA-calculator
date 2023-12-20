2cd5e2a4bb1b6bd4e14bc9c9c4a110e0d4d4587d
Bernadette Chukwuedo <chukwuedoogochukwubernadette@gmail.com>
2023-12-20 22:32:18 +0100

    A GPA calculator

---
+++ main.py
@@ -0,0 +1,35 @@
+# A code used to calculate your cgpa
+number = int(input("Enter total number of courses: "))
+count = 0
+count_score = 0
+count_UnitLoad = 0
+score = []
+
+# using a while condition to loop through
+while count != number:
+    grade = input("Enter grade: ")
+    if grade == "A":
+        score = 5
+    elif grade == "B":
+        score = 4
+    elif grade == "C":
+        score = 3
+    elif grade == "D":
+        score = 2
+    elif grade == "E":
+        score = 1
+    elif grade == "F":
+        score = 0
+    else:
+        print("invalid grade")
+
+    unit_load = int(input("Enter the unit load: "))
+    # calculating grades
+    gpa = score * unit_load
+    count_score += gpa
+    count_UnitLoad += unit_load
+    count += 1
+
+total_gpa = round(count_score / count_UnitLoad, 2)
+
+print(f"Your CGPA is {total_gpa}")
